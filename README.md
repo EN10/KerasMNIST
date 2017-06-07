b6c20bf39ab447d593e01291990aeb6173cd0ad1
Enio Fernandes <ef@cic.ac>
2017-06-07 20:10:02 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Keras MNIST
+
+https://github.com/fchollet/keras/blob/master/examples/mnist_mlp.py
+
+## Install
+
+    sudo pip install -U pip  
+    sudo pip install tensorflow 
+    
+disable "cpu_feature_guard":  
+`export TF_CPP_MIN_LOG_LEVEL=2`
---
+++ mnist.py
@@ -0,0 +1,43 @@
+from tensorflow.contrib.keras.python import keras
+from tensorflow.contrib.keras.python.keras.datasets import mnist
+from tensorflow.contrib.keras.python.keras.models import Sequential
+from tensorflow.contrib.keras.python.keras.layers import Dense
+from tensorflow.contrib.keras.python.keras.optimizers import SGD
+
+batch_size = 128
+num_classes = 10
+epochs = 20
+
+# the data, shuffled and split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Dense(10, activation='softmax', input_shape=(784,)))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=SGD(),
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
