5ab322cdc73254e39eb06b355f7c64ba82d720e9
Enio Fernandes <EN10@users.noreply.github.com>
2020-10-11 17:26:32 +0100

    Update TF2 MNIST.py

--- TF2 MNIST.py
+++ TF2 MNIST.py
@@ -1,19 +1,21 @@
+"""MNIST
+Code based on :
+https://www.tensorflow.org/overview
+"""
+
 import keras
 (train_images, train_labels), (test_images, test_labels) = keras.datasets.mnist.load_data()
 
-train_images = train_images / 255.0
-test_images = test_images / 255.0
+train_images, test_images = train_images / 255.0, test_images / 255.0
 
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(10, activation='relu')
+    keras.layers.Dense(10, activation='softmax')
 ])
 
 model.compile(optimizer='adam',
-              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels,
-                    epochs=10,
-                    verbose=1,
-                    validation_data=(test_images, test_labels))
+model.fit(train_images, train_labels, epochs=5)
+model.evaluate(test_images, test_labels)
